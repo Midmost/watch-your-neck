e0a7696adda662d2cf92eb690db9e4187cb1657b
4923 <4923.py@gmail.com>
2022-06-05 08:39:38 +0900

    [Chore] PWA 추가용 view 주석처리

--- project/app_turtle/views.py
+++ project/app_turtle/views.py
@@ -1,3 +1,22 @@
+from re import template
 from django.shortcuts import render
+from django.conf.urls import include
+from django.urls import reverse_lazy
+from django.http import HttpResponse
 
-# Create your views here.
+from . import app_settings
+
+def view_turtle(request):
+    return render(request, "app_turtle/checkpose.html")
+
+# PWA
+# def service_worker(request):
+#     response = HttpResponse(open(app_settings.PWA_SERVICE_WORKER_PATH).read(), content_type='application/javascript')
+#     return response
+
+# def manifest(request):
+#     return render(request, 'manifest.json', {
+#         setting_name: getattr(app_settings, setting_name)
+#         for setting_name in dir(app_settings)
+#         if setting_name.startswith('PWA_')
+#     })
