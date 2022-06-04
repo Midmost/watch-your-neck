0846ae49915afb7db549fcc0c50b505f61ba614b
4923 <4923.py@gmail.com>
2022-06-05 08:57:33 +0900

    [Fix] collectstatic 문제 해결 (heroku)

--- project/project/settings.py
+++ project/project/settings.py
@@ -133,6 +133,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # PWA Settings
 # ref: https://pypi.org/project/django-pwa/
