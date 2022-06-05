eb8ab7d4b2b1f8c156c744c67b12b6d50f265eea
4923 <4923.py@gmail.com>
2022-06-05 09:21:26 +0900

    [Fix] collectstatic 문제 해결
    ROOT 없이 모으려고 해서 생긴 문제
    ref: https://devcenter.heroku.com/articles/django-assets

--- project/project/settings.py
+++ project/project/settings.py
@@ -132,11 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # PWA Settings
 # ref: https://pypi.org/project/django-pwa/
